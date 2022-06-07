9a9b3be95e7c0967df91cd243dcbc699cb3e270c
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-06-07 10:58:43 -0300

    Move asdf out of linuxbrew

--- config/zsh/zsh.after/asdf.zsh
+++ config/zsh/zsh.after/asdf.zsh
@@ -1,4 +1,9 @@
 OS="$(uname)"
 if [[ "$OS" == "Linux" ]]; then
-. /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh
+	if [ -f /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh ]; then
+		echo "DO NOT USE ASDF INSTALLED IN LINUXBREW"
+		echo "Not loading asdf!"
+	elif [ -f "$HOME/.asdf/asdf.sh" ]; then
+		. "$HOME/.asdf/asdf.sh"
+  fi
 fi
